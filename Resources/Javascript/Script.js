32368c89b6ecb5c4c2b710fea9e19faa3766a6f0
Grace Hephzibah M <63595093+Grace-Hephzibah@users.noreply.github.com>
2021-01-25 17:55:43 +0530

    Day 23 Updates
    Sticky Navigation

--- Resources/Javascript/Script.js
+++ Resources/Javascript/Script.js
@@ -1,6 +1,29 @@
 $(Document).ready(function(){
+  
+        $('.js--section-features').waypoint(function(direction) 
+        {
+            if (direction == "down") 
+            {
+                $('nav').addClass('sticky');
+            } 
+            else 
+            {
+                $('nav').removeClass('sticky');
+            }
+        }, 
+        {
+            offset: '60px;'
+        });
     
-    $('h1').click(function(){
-        $(this).css('background-color', "#fff");
-    })
+    
+
+    
+    /*
+    var waypoints = $('#handler-first').waypoint(function(direction) {
+      notify(this.element.id + ' hit 25% from top of window') 
+    }, {
+      offset: '25%'
+    }) */
+    
+
 })
